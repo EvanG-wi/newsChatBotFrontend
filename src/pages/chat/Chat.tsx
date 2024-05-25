0e19069d06f0fc712b9674fc85f3b62f23dc1df2
EvanG <56905829+EvanG-wi@users.noreply.github.com>
2024-05-25 12:01:01 -0500

    Update Title message

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -172,7 +172,7 @@ const Chat = () => {
                         <div className={styles.chatEmptyState}>
                             //<SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
                             <img src="./src/assets/ww2news.jpg" alt="A newspaper from 1945" style={{ width: '120px' }} />
-                            <h1 className={styles.chatEmptyStateTitle}>Chat with your data</h1>
+                            <h1 className={styles.chatEmptyStateTitle}>Chat with History (1945)</h1>
                             <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
                             <ExampleList onExampleClicked={onExampleClicked} useGPT4V={useGPT4V} />
                         </div>
