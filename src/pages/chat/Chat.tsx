7d11de3d25d1d76e95c85ebca10f66dbd6a7757f
EvanG <56905829+EvanG-wi@users.noreply.github.com>
2024-05-27 16:13:26 -0500

    Update default text prompt

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -239,7 +239,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. what is product FR-R92B-58?)"
+                            placeholder="Type a new question (e.g. what is happening in China?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                         />
